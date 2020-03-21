fa7ef0a67b696b4bcb979bbe88783be7dfae3976
Jeromeski <jeromegee@gmail.com>
2020-03-22 05:58:00 +0800

    initial commit

---
+++ public/scripts/bundle.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var name = 'Jen';
+console.log(name);
+
+var hangman = function hangman() {
+  myMethod();
+};
+
+var myMethod = function myMethod() {
+  return 'Testing';
+};
+
+console.log(hangman());
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const name = 'Jen'
+console.log(name)
+
+const hangman = () => {
+  myMethod()
+}
+
+const myMethod = () => {
+  return 'Testing'
+}
+
+console.log(hangman())
